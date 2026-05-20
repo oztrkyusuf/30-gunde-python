6f67f5e0a5b269eaec0517cb0979794a93715c73
Yusuf ÖZTÜRK <ysfoztrk37343@gmail.com>
2026-05-20 14:23:50 +0300

    7. Gün: 1. Tekrar

---
+++ Gün-07/main.py
@@ -0,0 +1,7 @@
+print("Hesap Makinesine Hoşgeldiniz")
+sayi1=float(input("1. Sayı:"))
+sayi2=float(input("2.Sayı:"))
+
+toplamsayi=sayi1+sayi2
+print("İşlem Başarıyla Gerçekleşti.")
+print("Sonucunuz: ",toplamsayi)
