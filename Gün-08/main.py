607315664e5d4bc1792d4083a84dd520654e5658
Yusuf ÖZTÜRK <ysfoztrk37343@gmail.com>
2026-05-21 18:14:53 +0300

    8. Gün: Karar Verme

---
+++ Gün-08/main.py
@@ -0,0 +1,12 @@
+sayi=5
+if sayi>0:
+    print("Sayi 0'dan buyuktur")
+else:
+    print("Sayi 0'dan kucuktur")
+
+
+sayi1=float(input("Sayi Giriniz: "))
+if sayi1>10:
+    print("Sayi 10'dan buyuktur")
+else:    
+    print("Sayi 10'dan kucuktur")    
