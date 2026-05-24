0a471eb4d8ebcb6229864bbb642d9ded01824a7a
Yusuf ÖZTÜRK <ysfoztrk37343@gmail.com>
2026-05-24 10:08:28 +0300

    10. Gün: Karşılaştırma Operatörleri

---
+++ Gün-10/main.py
@@ -0,0 +1,9 @@
+sayi=float(input("Sayi  Gir: "))
+if sayi<10:
+    print("Sayi 10'dan kucuk")
+elif sayi==10:
+    print("Sayi 10'a esit")
+elif sayi>10:
+    print("Sayi 10'dan buyuk")
+else:
+    print(sayi)        
