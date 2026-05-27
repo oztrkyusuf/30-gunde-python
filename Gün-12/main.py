c25866f4ed1ef293fa929a7b86357b6ccc6dcc04
Yusuf ÖZTÜRK <ysfoztrk37343@gmail.com>
2026-05-27 17:19:23 +0300

    12. Gün: İç İçe if Yapısı

---
+++ Gün-12/main.py
@@ -0,0 +1,9 @@
+sayi=float(input("Sayi Gir: "))
+if sayi>0 and sayi<100:
+    print("1. Dongu Calisti")
+    isim=10
+    print(type(isim))
+    if  isim==10:
+        print("2. Dongude Calisti")
+else:
+    print("Sectigin sayi 0 ile 100 arasinda degil")
